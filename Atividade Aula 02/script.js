7f68432f24cd04960a4e4eed2e6861fd71528fc1
Daniel1702 <106408440+Daniel1702@users.noreply.github.com>
2022-10-25 21:15:00 -0300

    Add files via upload

---
+++ Atividade Aula 02/script.js
@@ -0,0 +1,33 @@
+/* Crie o seguinte array: var deuses = ['Aegir', 'Aud', 'Balder']
+Adicione os 3 Deuses: ('Loki', 'Odin', 'Frey') com a função push()
+
+Crie o seguinte array: var deuses = ['Aegir', 'Aud', 'Balder']
+Adicione os 3 Deuses: ('Loki', 'Odin', 'Frey') com a função .length
+
+Crie o seguinte array: var numbers = [5, 7, 1, 8, 9]
+E exiba de forma crescente dica: .sort
+
+Crie o seguinte array: var numbers = [5, 7, 1, 8, 9]
+E exiba de forma decrescente. */ 
+
+// ---------------------------------------------------------------------------------------
+
+// criando uma array
+var deuses =  ['Aegir,', 'Aud', 'Balder'];
+
+// adicionando mais valores com a função 'Push'
+deuses.push('Loki', 'Odin', 'Frey');
+console.log(deuses);
+
+// Criando uma nova array 
+var numbers = [5, 7, 1, 8, 9];
+
+// Para exibir de forma crescente com .sort
+numbers.sort(); 
+console.log(numbers);
+
+// Criando uma nova array 
+var numbers = [5, 7, 1, 8, 9];
+
+numbers.reverse();
+console.log(numbers);
